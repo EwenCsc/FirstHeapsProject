f06d7e463cc4e54098d4bdf045452be441fe5efc
Ewen Cossec <cossec.ewen@gmail.com>
2020-01-27 19:32:17 +0100

    rename assets

--- src/haxe_helper/Delegate.hx
+++ src/haxe_helper/Delegate.hx
@@ -2,8 +2,13 @@ package haxe_helper;
 
 // abstract Delegate<T>(Array<Dynamic>) {
 class Delegate<T> {
-        
-    public function new(func:Array<Dynamic> -> Void) {
-        
+    
+    private var callbacks : List<Array<Dynamic> -> T>;
+    
+    public function new(?_callbacks : List<Array<Dynamic> -> T>) {
+        if (_callbacks != null)
+            callbacks = _callbacks;
+        else 
+            callbacks = new List<Array<Dynamic> -> T>();
     }
 }
